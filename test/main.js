540a2022e9310a120692e093aebd563997606a15
Yamil Asusta <yamil@rebelmail.com>
2015-03-23 17:54:40 -0400

    Version bump

--- test/main.js
+++ test/main.js
@@ -5,7 +5,7 @@ var should = require('should');
 
 describe('html-autoprefixer', function() {
   it('version should be set', function() {
-    htmlAutoprefixer.version.should.eql('0.2.0');
+    htmlAutoprefixer.version.should.eql('0.2.1');
   });
 
   describe('.process().html', function() {
