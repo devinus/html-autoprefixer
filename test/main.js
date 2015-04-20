ed94db40eaf53db86fcceb2833fd630d566d22f6
Devin Torres <devin@devintorr.es>
2015-04-20 17:07:02 -0500

    Performance improvements

--- test/main.js
+++ test/main.js
@@ -1,43 +1,41 @@
 'use strict';
 
-var htmlAutoprefixer = require('../lib/main');
-var should = require('should');
+var expect = require('chai').expect;
+
+var htmlAutoprefixer = require('../');
 
 describe('html-autoprefixer', function() {
   describe('#process', function() {
-    it( 'autoprefixes inside of style tags', function( ) {
+    it('autoprefixes inside of style tags', function( ) {
       var htmlString = '<html><style>:fullscreen a { transition: transform 1s; }</style></html>';
       var prefixedResult = '<html><style>:-webkit-full-screen a { -webkit-transition: -webkit-transform 1s; transition: transform 1s; }\n:-moz-full-screen a { transition: transform 1s; }\n:-ms-fullscreen a { transition: transform 1s; }\n:fullscreen a { -webkit-transition: -webkit-transform 1s; transition: transform 1s; }</style></html>';
 
       var prefixed = htmlAutoprefixer.process(htmlString);
-      prefixed.should.eql(prefixedResult);
+      expect(prefixed).to.equal(prefixedResult);
     });
 
-    it( 'autoprefixes nested elements inside of style tags', function( ) {
+    it('autoprefixes nested elements inside of style tags', function( ) {
       var htmlString = '<html><style>@media screen and (max-width: 600px){ .class{ transition: transform 1s; } }</style></html>';
       var prefixedResult = '<html><style>@media screen and (max-width: 600px){ .class{ -webkit-transition: -webkit-transform 1s; transition: transform 1s; } }</style></html>';
 
       var prefixed = htmlAutoprefixer.process(htmlString);
-
-      prefixed.should.eql(prefixedResult);
+      expect(prefixed).to.equal(prefixedResult);
     });
 
-    it( 'autoprefixes nested elements of when doing SVG hack', function( ) {
+    it('autoprefixes nested elements of when doing SVG hack', function( ) {
       var htmlString = '<html><style>@media screen{ @media screen{ .svg{ transition: transform 1s; } } }</style></html>';
       var prefixedResult = '<html><style>@media screen{ @media screen{ .svg{ -webkit-transition: -webkit-transform 1s; transition: transform 1s; } } }</style></html>';
 
       var prefixed = htmlAutoprefixer.process(htmlString);
-
-      prefixed.should.eql(prefixedResult);
+      expect(prefixed).to.equal(prefixedResult);
     });
 
     it('autoprefixes inside of style attributes', function() {
-      var htmlString = '<html><h1 style="transition: transform 1s">Hello</h1></html>';
-      var prefixedResult = '<html><h1 style="-webkit-transition: -webkit-transform 1s;transition: transform 1s">Hello</h1></html>';
+      var htmlString = '<html><h1 style="transition: transform 1s">Hello</h1><h2 style="display: flex">World</h2></html>';
+      var prefixedResult = '<html><h1 style=\"-webkit-transition: -webkit-transform 1s;transition: transform 1s\">Hello</h1><h2 style=\"display: -webkit-box;display: -webkit-flex;display: -ms-flexbox;display: flex\">World</h2></html>';
 
       var prefixed = htmlAutoprefixer.process(htmlString);
-
-      prefixed.should.eql(prefixedResult);
+      expect(prefixed).to.equal(prefixedResult);
     });
 
     it('can pass cheerio options', function() {
@@ -45,11 +43,7 @@ describe('html-autoprefixer', function() {
       var prefixedResult = '<html><body>Hello</body></html>';
 
       var prefixed = htmlAutoprefixer.process(htmlString, { lowerCaseTags: true });
-
-      prefixed.should.eql(prefixedResult);
-
+      expect(prefixed).to.equal(prefixedResult);
     });
-
   });
-
 });
